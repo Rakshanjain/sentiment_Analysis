3b9317468a02ef43e100e44827382dcc96c3ab5c
Rakshan A Jain <55127137+Rakshanjain@users.noreply.github.com>
2020-10-03 21:29:32 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -4,17 +4,17 @@ import numpy as np
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
-
-df = pd.read_csv('/content/drive/My Drive/Machine learning/csv files/sentiment_review.csv')
-x = df.iloc[:,0].values
-y = df.iloc[:,1].values
-st.title("Sentiment Analysis on Review")
+ 
+df = pd.read_csv('sentiment_review.csv')
+x = df.iloc[:,0].values # Review column as input
+y = df.iloc[:,1].values # Sentiment column as output
+st.title("Sentiment Analysis On Review")
 st.subheader('TFIFD Vectorizer')
 st.write('This project is based on Naive Bayes Classifier')
-
+ 
 text_model = Pipeline([('tfidf',TfidfVectorizer()),('model',MultinomialNB())])
 text_model.fit(x,y)
-message = st.text_area("Enter Text","type here...")
+message = st.text_area("Enter Text","Type Here ..")
 op = text_model.predict([message])
 if st.button("Predict"):
   st.title(op)
